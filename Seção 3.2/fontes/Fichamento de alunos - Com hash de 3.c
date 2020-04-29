7c42cf9bf5d8f6caff68627d5bf3037463083311
prof. Wagner Berto <54516340+profwagnerberto@users.noreply.github.com>
2020-04-28 23:22:39 -0300

    Alteração de nomes idades1,2 e3 para colunas 0,1,2

--- Seção 3.2/fontes/Fichamento de alunos - Com hash de 3.c
+++ Seção 3.2/fontes/Fichamento de alunos - Com hash de 3.c
@@ -29,32 +29,106 @@ int CarregarIdades(int *paramDadosIdade){
   return ++contaIdade;
 }
 
-float CalcularMediaIdades(int ParamTamanhoIdade,int *ParamDadosIdade){
-  float soma;
-  int i;
-  soma = 0;
-  for(i=0;i<ParamTamanhoIdade;i++)
-    soma += ParamDadosIdade[i];
-  return soma / ParamTamanhoIdade;
+char dadosNome[30][20];
+int CarregarNomes(){
+  FILE *fl;
+  int caracter;
+  int tamanho = 1000;
+  char temp[tamanho];
+  int contaCarac;
+  int contaNome;
+  fl = fopen("dadosNome.txt", "r");
+  contaCarac=0;
+  temp[0]='\0';
+  contaNome=0;
+  while((caracter = fgetc(fl)) != EOF){
+    if(caracter!=','){
+      temp[contaCarac]=caracter;
+      contaCarac++;
+    }else{
+      temp[contaCarac]='\0';
+      strcpy(dadosNome[contaNome],temp);
+      contaCarac=0;
+      temp[0]='\0';
+      contaNome++;
+    }
+  }        
+  temp[contaCarac]='\0';
+  strcpy(dadosNome[contaNome],temp);
+  fclose(fl);
+  return ++contaNome;
 }
 
-int FECalculoDeEnderecos(int ParamIdadeABuscar, float ParamMedia){
-  if(ParamIdadeABuscar < ParamMedia)
-    return 0;
-  else
-    return 1;
+struct aluno { 
+  int idade;
+  char primeiroNome[15];
+  struct aluno* prox; 
+};
+
+typedef struct aluno Aluno;
+
+int FEDivisao(int ParamIdade,int ParamTamanhoTabela){
+  int restoDivisaoInteira;
+  restoDivisaoInteira = ParamIdade % ParamTamanhoTabela;
+  return restoDivisaoInteira;
+}
+
+Aluno* inicializar (void) {
+    return NULL;
+}
+
+Aluno* inserir (Aluno* l, Aluno v) {
+  Aluno* novo = (Aluno*) malloc(sizeof(Aluno));
+  novo -> idade = v.idade;
+  strcpy(novo -> primeiroNome, v.primeiroNome);
+  novo -> prox = l;
+  return novo;
+}
+
+void imprimir (Aluno* l) {
+  Aluno* p;
+  for (p = l; p != NULL; p = p -> prox) {
+    printf(" %d,%s -> ", p -> idade, p -> primeiroNome);
+  }
+  printf("NULL\n");
+}
+
+Aluno* buscar(Aluno* l, Aluno v){
+  Aluno* p;
+  for (p = l; p != NULL; p = p -> prox) {
+    if ( p -> idade == v.idade )
+      return p;
+  }
+  return NULL;
+}
+
+Aluno* remover (Aluno* l, Aluno v) {
+  Aluno* anterior = NULL;
+  Aluno* p = l;
+  while (p != NULL && p -> idade != v.idade) {
+    anterior = p;
+    p = p -> prox;
+  }
+  if (p == NULL)
+    return l;
+  if (anterior == NULL) 
+    l = p -> prox;
+  else 
+    anterior -> prox = p -> prox;
+  return l;
 }
 
 int main() {
   int opcao;
-  int idade;
-  int i;
   int dadosIdade[20];
   int tamanhoIdade;
+  int tamanhoNome;
+  Aluno umAluno;
   int endereco;
-  float media;
-  int tamanhoColuna[2];
-  int tabelaHash[10][2];
+  int i;
+  int tamanhoTabela = 3;
+  Aluno* tabelaHash[tamanhoTabela];
+  Aluno* alunoRetornado;
   int continuar;
   do{
     for(i=0;i<30;i++)
@@ -64,11 +138,12 @@ int main() {
     printf("\t MENU \n");
     printf("\t ---- \n");
     printf("\t 1-Carregar dados da idade e do nome dos alunos da turma \n");
-    printf("\t 2-Calcular a média das idades \n");
-    printf("\t 3-Obter endereço da tabela através da função de espalhamento na forma de Cálculo de Endereços \n");
+    printf("\t 3-Obter endereço da tabela através da função de espalhamento na forma de Divisão \n");
     printf("\t 4-Armazenar dados da tabela de idade e nome na Tabela de Espalhamento \n");
     printf("\t 5-Imprimir tabela \n");
-    printf("\t 6-Buscar na tabela\n");
+    printf("\t 6-Buscar na tabela \n");
+    printf("\t 7-Inserir um aluno na tabela \n");
+    printf("\t 8-Remover um aluno da tabela \n");
     printf("\t 9-Encerrar \n");
     printf("\t ---- \n");
     printf("\t Informe a opção desejada: ");
@@ -76,62 +151,72 @@ int main() {
     switch(opcao){
       case 1:
         tamanhoIdade = CarregarIdades(dadosIdade);
+        tamanhoNome = CarregarNomes();
         printf(" * \n Dados carregados. \n");
-        printf(" * \n * \n * \n");
-        break;
-      case 2:
-        media = CalcularMediaIdades(tamanhoIdade,dadosIdade);
-        printf(" * \n Média: %.2f \n",media);
         printf(" * \n * \n * \n");
         break;
       case 3:
         printf(" * \n Informe a idade a Calcular: ");
-        scanf("%d",&idade);
-        media = CalcularMediaIdades(tamanhoIdade,dadosIdade);
-        endereco = FECalculoDeEnderecos(idade,media);
+        scanf("%d",&umAluno.idade);
+        endereco = FEDivisao(umAluno.idade,tamanhoTabela);
         printf(" * \n Endereço Calculado: %d \n",endereco);
         printf(" * \n * \n * \n");
         break;
       case 4:
-        tamanhoColuna[0] = tamanhoColuna[1] = 0;
-        media = CalcularMediaIdades(tamanhoIdade,dadosIdade);
+        for(i=0;i<tamanhoTabela;i++)
+          tabelaHash[i] = inicializar();
         for(i=0;i<tamanhoIdade;i++){
-          endereco = FECalculoDeEnderecos(dadosIdade[i],media);
-          tabelaHash[tamanhoColuna[endereco]][endereco] = dadosIdade[i];
-          tamanhoColuna[endereco]++;
+          endereco = FEDivisao(dadosIdade[i],tamanhoTabela);
+          umAluno.idade = dadosIdade[i];
+          strcpy(umAluno.primeiroNome, dadosNome[i]);
+          tabelaHash[endereco] = inserir(tabelaHash[endereco],umAluno);
         }
         printf(" * \n Dados armazenados na Tabela de Espalhamento \n");
         printf(" * \n * \n * \n");
         break;
       case 5:
-        printf("índice \t idades1 \t idades2 \n");
-        printf("============================== \n");
-        for(i=0;i<10;i++){
-          printf("\t %d \t\t",i);
-          if(i<tamanhoColuna[0])
-            printf("%d \t\t\t",tabelaHash[i][0]);
-          if(i<tamanhoColuna[1])
-            printf("%d ",tabelaHash[i][1]);
+        printf("\n Tabela de espalhamento - MATRIZ \n");
+        for(i=0;i<tamanhoTabela;i++){
+          printf("Elementos da coluna %d \n",i);
+          printf("======== \n");
+          imprimir(tabelaHash[i]);
           printf("\n");
         }
         printf(" * \n * \n * \n");
         break;
       case 6:
         printf(" * \n Informe a idade a Buscar: ");
-        scanf("%d",&idade);
-        media = CalcularMediaIdades(tamanhoIdade,dadosIdade);
-        endereco = FECalculoDeEnderecos(idade,media);
-        i = 0;
-        while(i<tamanhoColuna[endereco] && tabelaHash[i][endereco]!=idade)
-          i++;
-        if(tabelaHash[i][endereco]==idade)
-          printf(" * \n A idade foi encontrada no vetor idades%d depois de %d iterações. \n",endereco+1,i+1);
-        else
+        scanf("%d",&umAluno.idade);
+        endereco = FEDivisao(umAluno.idade,tamanhoTabela);
+        alunoRetornado = buscar(tabelaHash[endereco],umAluno);
+        if (alunoRetornado == NULL) {
           printf(" * \n Idade NÃO encontrada. \n");
+        } else {
+          printf(" * \n A idade do aluno %s foi encontrada na coluna %d. \n",alunoRetornado->primeiroNome,endereco);
+        }
+        printf(" * \n * \n * \n");
+        break;
+      case 7:
+        printf(" * \n Informe a idade do aluno: ");
+        scanf("%d",&umAluno.idade);
+        fflush(stdin);
+        printf(" * \n Informe o nome do aluno: ");
+        scanf("%s",umAluno.primeiroNome);
+        endereco = FEDivisao(umAluno.idade,tamanhoTabela);
+        tabelaHash[endereco] = inserir(tabelaHash[endereco],umAluno);
+        printf(" * \n Aluno inserido na coluna %d \n",endereco);
+        printf(" * \n * \n * \n");
+        break;
+      case 8:
+        printf(" * \n Informe a idade do aluno: ");
+        scanf("%d",&umAluno.idade);
+        endereco = FEDivisao(umAluno.idade,tamanhoTabela);
+        tabelaHash[endereco] = remover(tabelaHash[endereco],umAluno);
+        printf(" * \n Aluno removido da coluna %d \n",endereco);
         printf(" * \n * \n * \n");
         break;
       case 9:
-        printf("Encerrando... \n");
+        printf(" * \n Encerrando... \n");
         printf(" * \n * \n * \n");
         break;
       default:
